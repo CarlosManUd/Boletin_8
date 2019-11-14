9affc6983fa5a2e3b714f4a09787252b8cca2602
Carlos Figueiras Valverde <cfigueirasvalverde@danielcastelao.org>
2019-11-14 13:07:59 +0100

    boletin_8

---
+++ Boletin_8/src/boletin_8/Boletin_8.java
@@ -0,0 +1,14 @@
+package boletin_8;
+
+/**
+ *
+ * @author cfigueirasvalverde
+ */
+public class Boletin_8 {
+
+ public static void main(String[] args) {
+        Suma obx = new Suma();
+        obx.setCantidad(obx.teclado);
+        obx.porcentajes();
+    }
+}
